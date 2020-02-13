e0ba1ee0883e46e44cc12099f38af337dd9e6a74
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-02-14 06:20:15 +0700

    bekerja dengan array

--- 08-fungsi-bagian-3-rekursif.php
+++ 08-fungsi-bagian-3-rekursif.php
@@ -33,3 +33,56 @@ function faktorial ($n) {
 
 $hasil = faktorial(5);
 echo $hasil;
+
+echo "<hr>";
+
+function tampilkanMenuBertingkat (array $menu) {
+  echo "<ul>";
+  foreach ($menu as $key => $item) {
+    echo "<li>{$item['nama']}</li>";
+
+    # periksa apakah ia memiliki atribut subMenu
+    # dan apakah attribut tersebut memiliki isi
+    if (@$item['subMenu'] && count($item['subMenu'])) {
+      # jika ia panggil diri sendiri
+      tampilkanMenuBertingkat($item['subMenu']);
+    }
+  }
+  echo "</ul>";
+}
+
+$menu = [
+  [
+    "nama" => "Beranda"
+  ],
+  [
+    "nama" => "Berita",
+    "subMenu" => [
+      [
+        "nama" => "Olahraga",
+        "subMenu" => [
+          [
+            "nama" => "Bola"
+          ],
+          [
+            "nama" => "Bulu Tangkis"
+          ]
+        ]
+      ],
+      [
+        "nama" => "Politik"
+      ],
+      [
+        "nama" => "Manca Negara"
+      ]
+    ]
+  ],
+  [
+    "nama" => "Tentang"
+  ],
+  [
+    "nama" => "Kontak"
+  ],
+];
+
+tampilkanMenuBertingkat($menu);
---
+++ 10-bekerja-dengan-array.php
@@ -0,0 +1,75 @@
+<?php
+
+# asosiatif array
+$listBuah = [
+  '0' => 'Anggur',
+  '1' => 'Jeruk',
+  '2' => 'Naga',
+  '3' => 'Duku'
+];
+
+var_dump(array_values($listBuah));
+
+echo "<hr>";
+
+$distroLinux = ['Ubuntu', 'Arch Linux', 'Elementary OS'];
+
+var_dump(array_search('Arch Linux', $distroLinux));
+// 1
+
+$mahasiswa = [
+  'nama' => 'Lendis Fabri',
+  'asal' => 'Lamongan',
+  'status_pernikahan' => 'Belum menikah',
+  'kewarganegaraan' => 'Indonesia'
+];
+
+var_dump(array_search('Lamongan', $mahasiswa));
+// 'asal'
+
+echo "<hr>";
+
+$nilaiSiswa = [
+  ['nama' => 'Andi', 'nilai' => 80],
+  ['nama' => 'Budi', 'nilai' => 40],
+  ['nama' => 'Candra', 'nilai' => 20],
+  ['nama' => 'Denis', 'nilai' => 70],
+  ['nama' => 'Fabrian', 'nilai' => 100],
+  ['nama' => 'Gunawan', 'nilai' => 90],
+  ['nama' => 'Hendra', 'nilai' => 30],
+  ['nama' => 'Ian', 'nilai' => 75],
+];
+
+# nilai yang kurang dari atau sama dengan 30
+$nilai30Kebawah = array_filter($nilaiSiswa, function ($item) {
+  return $item['nilai'] <= 30;
+});
+
+# nilai yang lebih dari 80
+$nilaiYangLebihDari80 = array_filter($nilaiSiswa, function ($item) {
+  return $item['nilai'] <= 30;
+});
+
+# kita juga bisa menggunakan fungsi panah
+$nilaiDiatas30Dan80Kebawah = array_filter($nilaiSiswa, fn($item) => $item['nilai'] > 30 and $item['nilai'] <= 80);
+
+echo "<h3 style='color: red'>Nilai 30 kebawah</h3>";
+
+foreach ($nilai30Kebawah as $item) {
+  echo "Nama: {$item['nama']} <br>";
+  echo "Nilai: {$item['nilai']} <br><br>";
+}
+
+echo "<h3 style='color: red'>Nilai lebih dari 80</h3>";
+
+foreach ($nilaiYangLebihDari80 as $item) {
+  echo "Nama: {$item['nama']} <br>";
+  echo "Nilai: {$item['nilai']} <br><br>";
+}
+
+echo "<h3 style='color: red'>Nilai dari 31 sampai 80</h3>";
+
+foreach ($nilaiDiatas30Dan80Kebawah as $item) {
+  echo "Nama: {$item['nama']} <br>";
+  echo "Nilai: {$item['nilai']} <br><br>";
+}
