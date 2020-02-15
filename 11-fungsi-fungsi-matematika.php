ee3adc9aa7b8c38ec99fa5d4a602a6c8727bf3ca
Ibnu Jakaria <ibnujakaria@outlook.com>
2020-02-15 08:15:53 +0700

    fungsi-fungsi matematika

---
+++ 11-fungsi-fungsi-matematika.php
@@ -0,0 +1,38 @@
+<?php
+
+echo "2 pangkat 2 = " . pow(2, 2) . "<br>";
+echo "2 pangkat 3 = " . pow(2, 3) . "<br>";
+echo "5 pangkat 3 = " . pow(5, 3) . "<br>";
+echo "3 pangkat 5 = " . pow(3, 5) . "<br>";
+
+echo "<hr>";
+
+echo "Akar 0 = " . sqrt(0) . "<br>";
+echo "Akar 1 = " . sqrt(1) . "<br>";
+echo "Akar 4 = " . sqrt(4) . "<br>";
+echo "Akar 9 = " . sqrt(9) . "<br>";
+
+echo "<hr>";
+
+echo deg2rad(120) . '<br>';
+echo deg2rad(30) . '<br>';
+echo deg2rad(225) . '<br>';
+
+echo '<hr>';
+
+echo round(4.6789) . "<br>";
+echo round(4.3234) . "<br>";
+echo round(pi(), 2) . "<br>"; # kita bulatkan nilai pi
+echo round(pi(), 4) . "<br>";
+
+echo '<hr>';
+
+echo floor(3.9999) . '<br>';
+echo floor(3.33) . '<br>';
+
+echo '<hr>';
+
+echo ceil(3.9999) . '<br>';
+echo ceil(3.33) . '<br>';
+
+echo '<hr>';
